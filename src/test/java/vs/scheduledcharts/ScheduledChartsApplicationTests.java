bfe99be2304db11f6af825177698d307ab1c67a7
Vasilis Souvatzis <vasouv@gmail.com>
2020-12-13 13:16:46 +0200

    setup

---
+++ src/test/java/vs/scheduledcharts/ScheduledChartsApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.scheduledcharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledChartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
