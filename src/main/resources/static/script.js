3968fbc17581a6e4d80a57f4bec02f0b04a30cc7
Vasilis Souvatzis <vasouv@gmail.com>
2021-02-13 22:31:18 +0200

    Update script.js
    creates the chart on each page reload

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,34 +1,52 @@
-window.addEventListener('load', (event) => {
-    fetch('http://localhost:8080/votes')
-        .then(response => response.json())
-        .then(data => console.log(data));
-
-});
-
 let candidates = ["Candidate A", "Candidate B"];
-let votes = [148, 123];
 let colors = ["#49A9EA", "#36CAAB"];
 
-let pollChart = document.getElementById('pollChart').getContext('2d');
-let chart = new Chart(pollChart, {
-    type: 'horizontalBar',
+// asynchronously calls the backend and returns the response
+async function getVotes() {
+    let response = await fetch("http://localhost:8080/votes");
+    let data = await response.json();
+    return data;
+}
 
-    data: {
-        labels: candidates,
-        datasets: [{
-            data: votes,
-            backgroundColor: colors
-        }]
-    },
+// creates the chart
+async function createChart() {
 
-    // Configuration options go here
-    options: {
-        title: {
-            text: "Total number of votes",
-            display: true
+    // call the API
+    let votesResult = await getVotes();
+
+    // get the total number of votes
+    let totalVotes = await votesResult.total;
+
+    // create an array with the number of votes for each candidate
+    let votes = await [votesResult["candidate a"], votesResult["candidate b"]];
+
+    let pollChart = document.getElementById('pollChart').getContext('2d');
+    let chart = new Chart(pollChart, {
+        type: 'horizontalBar',
+        data: {
+            labels: candidates,
+            datasets: [{
+                data: votes,
+                backgroundColor: colors
+            }]
         },
-        legend: {
-            display: false
+
+        // Configuration options go here
+        options: {
+            title: {
+                text: `Total number of votes: ${totalVotes}`,
+                display: true
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    offset: true
+                }]
+            }
         }
-    }
-});
+    });
+}
+
+createChart();
